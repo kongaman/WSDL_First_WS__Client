afc09424421f59a11a34cef52df26ae757566661
kongaman <christian.kranz@gmail.com>
2018-12-19 13:45:30 +0100

    create service

--- src/main/java/com/ckranz/ws/soap/CustomerOrderWSClient.java
+++ src/main/java/com/ckranz/ws/soap/CustomerOrderWSClient.java
@@ -1,9 +1,12 @@
 package com.ckranz.ws.soap;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class CustomerOrderWSClient {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws MalformedURLException {
+		CustomerOrderWsImplService service = new CustomerOrderWsImplService(new URL("http://localhost:8080/wsdlfirstws/customerordersservice?wsdl"));
 
 	}
 
