605de8ed562b333536d367de05d0f818bb6cc886
kongaman <christian.kranz@gmail.com>
2018-12-19 13:51:14 +0100

    create request

--- src/main/java/com/ckranz/ws/soap/CustomerOrderWSClient.java
+++ src/main/java/com/ckranz/ws/soap/CustomerOrderWSClient.java
@@ -1,15 +1,22 @@
 package com.ckranz.ws.soap;
 
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import com.bharath.ws.trainings.CustomerOrdersPortType;
+import com.bharath.ws.trainings.GetOrdersRequest;
+import com.bharath.ws.trainings.GetOrdersResponse;
 
 public class CustomerOrderWSClient {
 
 	public static void main(String[] args) throws MalformedURLException {
 		CustomerOrderWsImplService service = new CustomerOrderWsImplService(new URL("http://localhost:8080/wsdlfirstws/customerordersservice?wsdl"));
 		CustomerOrdersPortType customerOrderWsImplPort = service.getCustomerOrderWsImplPort();
+		
+		GetOrdersRequest request = new GetOrdersRequest();
+		request.setCustomerId(BigInteger.valueOf(1));
+		
 	}
 
 }
