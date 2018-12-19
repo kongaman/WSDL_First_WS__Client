3eb5ba0fc2ced2e3dd1bbda21754fa141a67e059
kongaman <christian.kranz@gmail.com>
2018-12-19 13:46:52 +0100

    create porttype

--- src/main/java/com/ckranz/ws/soap/CustomerOrderWSClient.java
+++ src/main/java/com/ckranz/ws/soap/CustomerOrderWSClient.java
@@ -3,11 +3,13 @@ package com.ckranz.ws.soap;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import com.bharath.ws.trainings.CustomerOrdersPortType;
+
 public class CustomerOrderWSClient {
 
 	public static void main(String[] args) throws MalformedURLException {
 		CustomerOrderWsImplService service = new CustomerOrderWsImplService(new URL("http://localhost:8080/wsdlfirstws/customerordersservice?wsdl"));
-
+		CustomerOrdersPortType customerOrderWsImplPort = service.getCustomerOrderWsImplPort();
 	}
 
 }
